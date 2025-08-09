23fc0548885014cf8bd8f0a2ee19d47b3696c860
abdulsrevenge <abdulghalibagboola@gmail.com>
2025-08-09 15:05:37 -0500

    Update ui.js

--- scripts/ui.js
+++ scripts/ui.js
@@ -214,7 +214,7 @@ function wireUI(config, tracking) {
   $('#app-root').on('click', '#call-tab', function(e){ e.preventDefault(); switchTab('#call-data'); });
 }
 
-export function showPage(key) { {
+export function showPage(key) {
   $('#website-panel > div').hide();
   const mapping = {
     'search': '#search-page',
